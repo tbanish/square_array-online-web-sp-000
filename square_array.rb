a0d088d26d3631c853c5c6ca672fc2e4dd1e2972
Tom Banish <tombanish@gmail.com>
2019-11-24 00:58:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,7 @@
 def square_array(array)
   new_array = []
-  array.each do {|number| new_array << number ** 2} 
-  new_array
+    array.each do {|number| new_array << number ** 2} 
+      new_array
+    end
+  end
 end
